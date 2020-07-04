ecc000bb7e6094a4f9102c7a7b38968e93f17d6a
Dylan Curzon <d.46.curzon@hotmail.co.uk>
2020-07-04 03:33:56 +0100

    maybe this was useful

--- lib/config.ex
+++ lib/config.ex
@@ -1,5 +1,7 @@
 defmodule Shiina.Config do
 
+  @type value :: %{raw: any, type: atom}
+
   defmodule Cache do
     use Agent
     def start_link() do
@@ -20,52 +22,30 @@ defmodule Shiina.Config do
 
   end
 
-  # def get(guild) do
-  #   get_document(guild)
-  # end
+  def get(guild_id, path \\ [], options \\ []) do
+    do_translate = Keyword.get(options, :translate, true)
 
-  def get(guild) do
-    get(guild, "")
+    document = find_document(guild_id)
+    value    = get_in(document, path)
+
+    with %{raw: raw, type: type} <- value
+    do
+      if do_translate do
+
+      else
+        raw
+      end
+    end
   end
 
-  def get(guild, "") do
-    get_document(guild)
+  def translate(raw, type) do
+    case type do
+      ""
+    end
   end
 
-  def get(guild, path) do
-    keys = String.split(path, ".")
-    document = get_document(guild)
-    Kernel.get_in(document, keys)
-  end
-
-  def exists(guild, path) do
-    get(guild, path) != nil
-  end
-
-  def set(guild, path, value) do
-    keys = String.split(path, ".")
-    document = get(guild)
-    document = Shiina.Helpers.put_in_safely(document, keys, value)
-    update_document(guild, document)
-    :ok
-  end
-
-  def unset(guild, path) do
-    keys = String.split(path, ".")
-    document = get(guild)
-    {_, document} = Kernel.pop_in(document, keys)
-    update_document(guild, document)
-    :ok
-  end
-
-  def reset(guild, to \\ nil) do
-    new_document = to || get_filter(guild)
-    update_document(guild, new_document)
-    {:ok, new_document}
-  end
-
-  defp get_document(guild) do
-    filter = get_filter(guild)
+  defp find_document(guild_id) do
+    filter = %{guild: guild_id}
     result = Mongo.find_one(:shiina, "guild-config", filter)
     case result do
       nil ->
@@ -75,13 +55,53 @@ defmodule Shiina.Config do
     end
   end
 
-  defp update_document(guild, document) do
-    filter = get_filter(guild)
-    Mongo.find_one_and_replace(:shiina, "guild-config", filter, document, upsert: true)
-  end
+  # def get(guild) do
+    # get(guild, "")
+  # end
 
-  defp get_filter(guild) do
-    %{guild: guild}
-  end
+  # def get(guild, "") do
+  #   get_document(guild)
+  # end
+
+  # def get(guild, path) do
+  #   keys = String.split(path, ".")
+  #   document = get_document(guild)
+  #   Kernel.get_in(document, keys)
+  # end
+
+  # def exists(guild, path) do
+  #   get(guild, path) != nil
+  # end
+
+  # def set(guild, path, value) do
+  #   keys = String.split(path, ".")
+  #   document = get(guild)
+  #   document = Shiina.Helpers.put_in_safely(document, keys, value)
+  #   update_document(guild, document)
+  #   :ok
+  # end
+
+  # def unset(guild, path) do
+  #   keys = String.split(path, ".")
+  #   document = get(guild)
+  #   {_, document} = Kernel.pop_in(document, keys)
+  #   update_document(guild, document)
+  #   :ok
+  # end
+
+  # def reset(guild, to \\ nil) do
+  #   new_document = to || get_filter(guild)
+  #   update_document(guild, new_document)
+  #   {:ok, new_document}
+  # end
+
+  # defp update_document(guild, document) do
+  #   filter = get_filter(guild)
+  #   Mongo.find_one_and_replace(:shiina, "guild-config", filter, document, upsert: true)
+  # end
+
+  # defp get_filter(guild) do
+  #   %{guild: guild}
+  # end
 
 end
